a6b6d110fea8018a54b63b6e453650ae2a43680f
mick-d97 <mickael.dolphin@live.fr>
2020-12-08 16:42:34 +0100

    ajout de l'exercice 8

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$result1 = 3+4;
+$result2 = 5*20;
+$result3 = 45/5;
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p><?=$result1?></p>
+    <p><?=$result2?></P>
+    <p><?=$result3?></p>
+</body>
+</html>
